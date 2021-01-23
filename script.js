c17febc63ef0f7758073593226a89c6384173eab
Alex1 <g-al@list.ru>
2021-01-23 11:52:33 +0300

    1

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/* const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = prompt('На сколько оцените его?', '');
-
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
- */
-for (let i= 0; i< 2; i++){
+for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
-     if (a !=null && b !=null &&  a !="" && b!="" && a.length <50 ){
-         personalMovieDB.movies[a] = b;
-         console.log('done');
-        }else{
-            console.log('error');
-        }
-
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-console.log(personalMovieDB);
+}

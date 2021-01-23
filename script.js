8eee2f5946126381a516d250e1fffbc1b59a6480
Alex1 <g-al@list.ru>
2021-01-23 13:52:39 +0300

    3

--- script.js
+++ script.js
@@ -39,6 +39,20 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 }
-*/
 
-console.log(personalMovieDB);
+*/
+let i = 0;
+do {const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('super'); i++;
+
+    } else {
+        console.log('error');
+        i--;
+    }
+    console.log(personalMovieDB);
+}
+while( i <2);
